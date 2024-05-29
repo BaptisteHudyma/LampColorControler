5493c1b88740cf980bb9ac123ebdc34e7abe1629
BaptHdm <bapt.hudyma95650@gmail.com>
2024-05-29 17:19:30 +0200

    Base code for CCT

--- src/user_constants.h
+++ src/user_constants.h
@@ -2,8 +2,8 @@
 #define USER_CONSTANTS
 
 // parameters of the led strip used
-constexpr float consWattByMeter = 0;  // power consumption (in Watt/meters)
-constexpr float inputVoltage_V = 12;  // voltage (volts)
-constexpr float ledStripLenght_mm = 0;
+constexpr float consWattByMeter = 10;  // power consumption (in Watt/meters)
+constexpr float inputVoltage_V = 12;   // voltage (volts)
+constexpr float ledStripLenght_mm = 67.0 * 27.0;  // 67 sections of 27 mm
 
 #endif
